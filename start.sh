fe45b955a06634ec0ec422969ad9b547d22e61e4
Fakhra Aufia <fahrmnr@gmail.com>
2025-06-10 09:15:31 +0700

    git commit -m "Add shield install and super-admin to start.sh"

--- start.sh
+++ start.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 # Make sure this file has executable permissions, run `chmod +x start.sh`
 # Run migrations, set up nginx conf and run nginx
+# Jalankan migrasi dan setup Shield
+php artisan migrate --force
+php artisan shield:install
+php artisan shield:super-admin
+
 php artisan migrate --force && node /assets/scripts/prestart.mjs /assets/nginx.template.conf /nginx.conf && (php-fpm -y /assets/php-fpm.conf & nginx -c /nginx.conf)
