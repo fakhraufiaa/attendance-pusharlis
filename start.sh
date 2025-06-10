49e4e9f172a0d54e07b3ea08059431831d50d773
Fakhra Aufia <fahrmnr@gmail.com>
2025-06-10 11:40:47 +0700

    update start.sh

--- start.sh
+++ start.sh
@@ -2,8 +2,17 @@
 # Make sure this file has executable permissions, run `chmod +x start.sh`
 # Run migrations, set up nginx conf and run nginx
 # Jalankan migrasi dan setup Shield
+#!/bin/bash
+
+# Jalankan perintah setup
 php artisan migrate --force
 php artisan shield:install
 php artisan shield:super-admin
+php artisan shield:generate
 
-php artisan migrate --force && node /assets/scripts/prestart.mjs /assets/nginx.template.conf /nginx.conf && (php-fpm -y /assets/php-fpm.conf & nginx -c /nginx.conf)
+# Start server Laravel (via Nginx & PHP-FPM)
+node /assets/scripts/prestart.mjs /assets/nginx.template.conf /nginx.conf
+php-fpm -y /assets/php-fpm.conf &
+nginx -c /nginx.conf
+
+# php artisan migrate --force && node /assets/scripts/prestart.mjs /assets/nginx.template.conf /nginx.conf && (php-fpm -y /assets/php-fpm.conf & nginx -c /nginx.conf)
